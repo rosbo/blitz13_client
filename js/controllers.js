24cb02bcce377b5da6cc4f0fb640a421a7194923
Vincent Roseberry <roseberryv@gmail.com>
2013-01-12 09:59:50 -0500

    tt

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,20 @@
 'use strict';
 
 function MainCtrl($scope) {
+    $scope.team = [
+    {
+        'name': 'Vincent Roseberry',
+        'url': 'http://www.vroseberry.com'
+    },{
+        'name': 'Sylvain Perron',
+        'url': 'http://www.vroseberry.com'
+    },{
+        'name': 'Simon-Pierre Gingras',
+        'url': 'http://www.vroseberry.com'
+    },{
+        'name': 'Francis Valois',
+        'url': 'http://www.vroseberry.com'
+    }];
+
+    
 }
